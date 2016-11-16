973bf004f0309bca13812de9a9de07c69617aabc
Sonik Jhang <sonik.jhang@utexas.edu>
2016-11-15 22:34:59 -0800

    working start button

--- entry.js
+++ entry.js
@@ -1,4 +1,7 @@
+/* eslint no-undef: "off", max-len: "off" */
+
 import Game from './lib/game';
+import Snow from './lib/snow';
 
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("canvas");
